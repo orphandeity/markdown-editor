09e54b485ca6b9855328a83602b37188b7682544
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-28 12:20:16 -0700

    updates globals.css.ts

--- src/styles/globals.css.ts
+++ src/styles/globals.css.ts
@@ -4,3 +4,37 @@ import { vars } from './theme.css'
 globalStyle('html, body', {
   fontFamily: vars.fontFamily.sans,
 })
+
+// reset styles inspired by Josh Comeau
+// https://www.joshwcomeau.com/css/custom-css-reset/
+globalStyle('*', {
+  margin: '0',
+  padding: '0',
+  lineHeight: 'calc(1em + 0.5rem)',
+})
+
+globalStyle('*::before, *::after', {
+  boxSizing: 'border-box',
+})
+
+globalStyle('body', {
+  WebkitFontSmoothing: 'antialiased',
+})
+
+globalStyle('input, button, textarea, select', {
+  font: 'inherit',
+})
+
+globalStyle('img, picture, video, canvas, svg', {
+  display: 'block',
+  maxWidth: '100%',
+})
+
+globalStyle('p, h1, h2, h3, h4, h5, h6', {
+  overflowWrap: 'break-word',
+})
+
+globalStyle(':root', {
+  // resets stacking context for react portals
+  isolation: 'isolate',
+})
