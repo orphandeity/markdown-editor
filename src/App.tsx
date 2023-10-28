d3c9b8c403bbb7bca11abb65848a11058628cff9
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-28 11:50:44 -0700

    updates App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 export default function App() {
   return (
-    <>
-      <h1>hello world!</h1>
-    </>
+    <div>
+      <div id="menu">
+        <b>markdown</b>
+        <h2>my documents</h2>
+        <button>+ new document</button>
+        <ul>
+          <li>
+            <div>created_at</div>
+            <div>filename</div>
+          </li>
+          <li>
+            <div>created_at</div>
+            <div>filename</div>
+          </li>
+        </ul>
+        <button>theme</button>
+      </div>
+      <div id="main">
+        <header>
+          <button>menu</button>
+          <span>filename</span>
+          <menu>
+            <button>delete</button>
+            <button>save</button>
+          </menu>
+        </header>
+        <article id="editor">
+          <main id="markdown">
+            <header>
+              <h2>markdown</h2>
+              <button>preview</button>
+            </header>
+            <textarea></textarea>
+          </main>
+          <aside id="preview">
+            <header>
+              <h2>preview</h2>
+              <button>close</button>
+            </header>
+            <textarea></textarea>
+          </aside>
+        </article>
+      </div>
+    </div>
   )
 }
