99ecb8f33301fbc70c7c0beff96fc012fb9a2bc8
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-30 00:12:05 -0700

    updates app.css.ts

--- src/styles/app.css.ts
+++ src/styles/app.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css'
 export const container = style({
   display: 'flex',
   transform: 'translateX(-225px)',
+  transition: 'all 0.4s ease-in-out',
   selectors: {
     '&[data-open="true"]': {
       transform: 'translateX(0)',
